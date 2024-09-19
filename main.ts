8979f245e31732aef1e0e9bb885e033d3c84cc8a
TanyaVerb <karra_t@mail.ru>
2024-09-19 23:32:21 +0300

    add function validateFormData

--- main.ts
+++ main.ts
@@ -65,6 +65,68 @@ formEl.addEventListener("submit", (e) => {
     checkFormData(formData);
   }
 });
+//===================================================================
+// interface IFormData {
+//     email: string;
+//     title: string;
+//     text: string;
+//     checkbox: boolean;
+// }
+
+// const formData: IFormData = {
+//     email: "",
+//     title: "",
+//     text: "",
+//     checkbox: false,
+// };
+// const form = document.querySelector('form') as HTMLFormElement;
+// const email = document.querySelector('#email') as HTMLInputElement;
+// const title = document.querySelector('#text') as HTMLInputElement;
+// const text = document.querySelector('#email') as HTMLTextAreaElement;
+// const ckeckbox = document.querySelector('#ckeckbox') as HTMLInputElement;
+
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     formData.email = email.value ?? ''
+//     formData.title = title.value ?? ''
+//     formData.text = text.value ?? ''
+//     formData.checkbox = email.checked ?? false
+
+//     if (validateFormData(formData)) {
+//         checkFormData(formData)
+//     }
+// })
+
+// // Последовательность действий:
+// // 1) Происходит submit любой из форм
+// // 2) Все данные из 4х полей со страницы переходят в свойства объекта formData
+// // 3) Запускается функция validateFormData с этим объектом, возвращает true/false
+// // 4) Если на предыдущем этапе true, то запускается функция checkFormData с этим объектом
+// function validateFormData(data: IFormData): boolean {
+//     const arrValues = Object.values(data)
+
+//     const isValid = arrValues.every((value) => value)
+
+//     if (isValid) {
+//         return true;
+//     } else {
+//         console.log("Please, complete all fields");
+//         return false;
+//     }
+// }
+
+// function checkFormData(data: IFormData): void {
+//     const { email } = data;
+//     const emails = ["example@gmail.com", "example@ex.com", "admin@gmail.com"];
+
+//     const isNotValid = emails.some((value) => value === email)
+//     // Если email совпадает хотя бы с одним из массива
+//     if (isNotValid) {
+//         console.log("This email is already exist");
+//     } else {
+//         console.log("Posting data...");
+//             }
+//         }
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // const formEl = document.getElementById("myForm") as HTMLFormElement;
